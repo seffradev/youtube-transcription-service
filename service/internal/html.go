bb7cc4a79fc1d4d05c3d4d3b5a48ee1a7c382155
Hampus Avekvist <hampus.avekvist@hey.com>
2024-03-09 23:35:35 +0100

    feat(service): use templates rather than hard coded html-pages

--- service/internal/html.go
+++ service/internal/html.go
@@ -5,12 +5,12 @@ import "github.com/gin-gonic/gin"
 func Html(engine *gin.Engine) {
 	engine.LoadHTMLGlob("./web/templates/*")
 	engine.GET("/", func(c *gin.Context) {
-		c.HTML(200, "index.html", gin.H{})
+		c.HTML(200, "index.tmpl", gin.H{})
 	})
 	engine.GET("/login", func(c *gin.Context) {
-		c.HTML(200, "login.html", gin.H{})
+		c.HTML(200, "login.tmpl", gin.H{})
 	})
 	engine.GET("/register", func(c *gin.Context) {
-		c.HTML(200, "register.html", gin.H{})
+		c.HTML(200, "register.tmpl", gin.H{})
 	})
 }
