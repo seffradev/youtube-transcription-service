8bcd7734481c3e3ed366822265b5624bc8b1f7dc
Hampus Avekvist <hampus.avekvist@hey.com>
2024-03-10 15:03:22 +0100

    feat(service): add database connection

--- internal/environment.go
+++ internal/environment.go
@@ -7,18 +7,19 @@ import (
 	"github.com/joho/godotenv"
 )
 
-func Environment() (string, string, error) {
+func Environment() (string, string, string, error) {
 	var err error
 
 	log.SetPrefix("[APP] ")
 
 	err = godotenv.Load("configs/webserver/.env")
 	if err != nil {
-		return "", "", err
+		return "", "", "", err
 	}
 
 	ip := os.Getenv("IP")
 	port := os.Getenv("PORT")
+	db_url := os.Getenv("DATABASE_URL")
 
-	return ip, port, err
+	return ip, port, db_url, err
 }
