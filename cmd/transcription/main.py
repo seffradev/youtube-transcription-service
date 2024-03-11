2597117dbb889b852c0b07dd5244ab30f795b99e
Hampus Avekvist <hampus.avekvist@hey.com>
2024-03-11 01:35:13 +0100

    feat(transcription): modularize transcription engine into smaller packages

--- cmd/transcription/main.py
+++ cmd/transcription/main.py
@@ -1,46 +1,11 @@
-import subprocess
-import sys
-import yt_dlp
-import whisper
+from transcribe import download_video, transcribe, remove_video
 
-
-def download_video(urls):
-    if len(urls) > 1:
-        print("Too many URLs. Can only handle one at a time")
-        sys.exit(1)
-    filename = ""
-    codec = "mp3"
-    ydl_opts = {
-        "format": "m4a/bestaudio/best",
-        "postprocessors": [{  # Extract audio using ffmpeg
-            "key": "FFmpegExtractAudio",
-            "preferredcodec": codec,
-        }],
-        "quiet": True,
-        "external_downloader_args": ["-loglevel", "panic"],
-        "noprogress": True,
-    }
-    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        error_code = ydl.download(urls)
-        info = ydl.extract_info(urls[0], download=False)
-        id = info["id"]
-        title = info["title"]
-        filename = f"{title} [{id}].{codec}"
-    return filename
-
-def transcribe(filename, language=None):
-    model = whisper.load_model("small")
-    result = model.transcribe(filename)
-    return result["text"]
+def main():
+    url = input("Enter the URL of the video: ")
+    filename = download_video([url])
+    transcription = transcribe(filename)
+    remove_video(filename)
+    print(transcription)
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print(f"usage: {sys.argv[0]} <url>")
-        sys.exit(1)
-    filename = download_video([sys.argv[1]])
-
-    transcription = transcribe(filename)
-
-    subprocess.run(["rm", filename])
-
-    print(transcription)
+    main()
