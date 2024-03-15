f3d0a064be688757a7be0a5869822cf968bd4bba
Hampus Avekvist <hampus.avekvist@hey.com>
2024-03-15 01:36:42 +0100

    feat(database): remove account from transcription

--- scripts/migrations/transcriptions/up.sql
+++ scripts/migrations/transcriptions/up.sql
@@ -1,10 +1,8 @@
 CREATE TABLE transcription (
     id VARCHAR(11) NOT NULL PRIMARY KEY ,
-    aid INT NOT NULL,
     status ENUM('pending', 'in_progress', 'completed', 'failed') NOT NULL,
     text TEXT,
     requested_at DATETIME NOT NULL DEFAULT now(),
     completed_at DATETIME,
     cost INT,
-    FOREIGN KEY (aid) REFERENCES account(id)
 );
