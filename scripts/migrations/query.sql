191da724f219d52bb42a33f51677fb991ed9a4d3
Hampus Avekvist <hampus.avekvist@hey.com>
2024-03-15 04:28:52 +0100

    feat(webserver): add sqlc support

---
+++ scripts/migrations/query.sql
@@ -0,0 +1,40 @@
+-- name: GetTranscription :one
+SELECT
+    id,
+    text,
+    status,
+    requested_at,
+    completed_at
+FROM
+    transcription
+WHERE
+    id = ?
+LIMIT 1;
+
+-- name: ListTranscription :many
+SELECT
+    id,
+    text,
+    status,
+    requested_at,
+    completed_at
+FROM
+    transcription;
+
+-- name: CreateTranscription :execresult
+INSERT INTO
+    transcription (id, status)
+VALUES (
+    ?,
+    'pending'
+);
+
+-- name: UpdateTranscription :execresult
+UPDATE
+    transcription
+SET
+    text = ?,
+    status = 'completed',
+    completed_at = now()
+WHERE
+    id = ?;
---
+++ scripts/migrations/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE transcription (
+    id VARCHAR(11) NOT NULL PRIMARY KEY ,
+    status ENUM('pending', 'in_progress', 'completed', 'failed') NOT NULL,
+    text TEXT,
+    requested_at DATETIME NOT NULL DEFAULT now(),
+    completed_at DATETIME
+);
