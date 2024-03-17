ecbc1c9ad0f89dbbead260310f10cd3d6073e00c
Hampus Avekvist <hampus.avekvist@hey.com>
2024-03-17 14:19:55 +0100

    feat(webserver): stop failing on lack of .env

--- internal/environment.go
+++ internal/environment.go
@@ -13,14 +13,11 @@ func Environment() (string, string, string, string, error) {
 	log.SetPrefix("[APP] ")
 
 	err = godotenv.Load("configs/webserver/.env")
-	if err != nil {
-		return "", "", "", "", err
-	}
 
 	ip := os.Getenv("IP")
 	port := os.Getenv("PORT")
-    bootstrapServers := os.Getenv("BOOTSTRAP_SERVERS")
-    databaseUrl := os.Getenv("DATABASE_URL")
+	bootstrapServers := os.Getenv("BOOTSTRAP_SERVERS")
+	databaseUrl := os.Getenv("DATABASE_URL")
 
 	return ip, port, bootstrapServers, databaseUrl, err
 }
